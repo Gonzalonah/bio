73caf904323e2bee2b40cbd545ca86f2e27193a4
Gonzalo Nahuel Rolón <gonnron@gmial.com>
2022-08-18 05:54:11 -0300

    magnetometeraaaaaaaa

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
-//import React from 'react';
-//import ReactDOM from 'react-dom';
-
-import time from './init.js'
-
-time()
+import { sayHi } from "./utils";
+sayHi();
--- src/init.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-export default function time () {
-  ReactDOM.render(
-    <h1 id="time">🖐Hola🤙</h1>, 
-    document.getElementById('app')
-    )};
