a873178bc4e1be531f8f1c6b27d206a804703c15
Gonzalo Nahuel Rolón <gonnron@gmial.com>
2022-08-17 23:19:26 -0300

    init

---
+++ src/index.js
@@ -0,0 +1,6 @@
+//import React from 'react';
+//import ReactDOM from 'react-dom';
+
+import time from './init.js'
+
+time()
---
+++ src/init.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function time () {
+  ReactDOM.render(
+    <h1 id="time">🖐Hola🤙</h1>, 
+    document.getElementById('app')
+    )};
